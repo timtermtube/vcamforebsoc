02c593cd36d90bb089074078bc5da119fd5084c4
timtermtube <79361415+timtermtube@users.noreply.github.com>
2021-03-21 23:04:52 +0900

    add Servers

---
+++ node/index.js
@@ -0,0 +1,58 @@
+/*new Peer("videoProvider", {
+    host: "timclass.kro.kr", port:20500, debug: 1, path: "/peer"
+})*/
+
+const fs = require("fs");
+const credit = {
+    ca: "fullchain please",
+    key: "private key please",
+    cert: "cert please"
+}
+const systemConfigs = {
+    port: 20500,
+    password: "tempPassword"
+};
+
+const express = require("express");
+const {ExpressPeerServer} = require("peer");
+const app = express();
+const https = require("https").createServer(credit, app);
+
+const peer = ExpressPeerServer(https, {
+    proxied: true,
+    debug: true,
+    path: "/peer",
+    ssl: {}
+});
+
+let lists = {
+    videoProvider: 0,
+    beProvidedCamera: 0
+}
+
+app.use(peer);
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+peer.on("connection", (c) => {
+    if (c.id == "videoProvider" && !lists.videoProvider) {
+        lists.videoProvider = c;
+    }
+    else if (c.id == "beProvidedCamera" && !lists.beProvidedCamera) {
+        lists.beProvidedCamera = c;
+    }
+});
+
+peer.on("disconnect", (c) => {
+    if (c.id == "videoProvider" && lists.videoProvider) {
+        lists.videoProvider = 0;
+    }
+    else if (c.id == "beProvidedCamera" && lists.beProvidedCamera) {
+        lists.beProvidedCamera = 0;
+    }
+})
+
+https.listen(systemConfigs.port, "0.0.0.0");
+console.log("Server is starting!")
